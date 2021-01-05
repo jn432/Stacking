97a73e1e3965399b5851ca7def5907b8584038ab
jn432 <jn432@uowmail.edu.au>
2021-01-05 16:43:48 +1100

    Signed-off-by: jn432 <jn432@uowmail.edu.au>

--- stacking.py
+++ stacking.py
@@ -29,7 +29,32 @@ min_y = min(y)
 max_y = max(y)
 y = (y - min_y) / (max_y - min_y)
 
-#%%
+#%% split data into training and test sets
+from sklearn.model_selection import train_test_split
 
-print('a')
-print('b')
+#using 80% of data for training, 20% for training
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+#%% random forest training
+from sklearn.ensemble import RandomForestRegressor
+rf = RandomForestRegressor(n_jobs=4, n_estimators=50)
+rf.fit(X_train, y_train)
+
+#%% evaluation on training set
+y_pred = rf.predict(X_train)
+
+#using mean squared error for evaluation
+from sklearn.metrics import mean_squared_error
+for i in range(len(y_train)):
+    print(y_train[i], y_pred[i])
+print(mean_squared_error(y_train, y_pred))
+    
+#%% evaluation on test set
+y_pred = rf.predict(X_test)
+
+for i in range(len(y_test)):
+    print(y_test[i], y_pred[i])
+print(mean_squared_error(y_test, y_pred))
+
+#%% gradient boosting training
+
