2cdd8c22176ccf95af397d80b8297fba09d23143
jn432 <jn432@uowmail.edu.au>
2021-01-06 14:21:43 +1100

    Added decision tree

--- stacking.py
+++ stacking.py
@@ -49,14 +49,12 @@ rf.fit(X_train, y_train)
 
 #%% evaluation on training set
 y_pred = rf.predict(X_train)
-
 #using mean squared error for evaluation
 from sklearn.metrics import mean_squared_error
 print('MSE train RF:', mean_squared_error(y_train, y_pred))
     
 #%% evaluation on test set
 y_pred = rf.predict(X_test)
-
 print('MSE test RF:', mean_squared_error(y_test, y_pred))
 
 
@@ -68,15 +66,33 @@ gb.fit(X_train, y_train)
 
 #%% evaluation on training set
 y_pred = gb.predict(X_train)
-
 #using mean squared error for evaluation
-from sklearn.metrics import mean_squared_error
 print('MSE train GB:', mean_squared_error(y_train, y_pred))
     
 #%% evaluation on test set
 y_pred = gb.predict(X_test)
-
 print('MSE test GB:', mean_squared_error(y_test, y_pred))
 
-#%% 3rd model(TBD - probably one better suited for outliers)
-#blah
+
+
+#%% decision tree
+from sklearn.tree import DecisionTreeRegressor
+dt = DecisionTreeRegressor(max_depth=5)
+dt.fit(X_train, y_train)
+
+#%% evaluation on training set
+y_pred = dt.predict(X_train)
+#using mean squared error for evaluation
+print('MSE train DT:', mean_squared_error(y_train, y_pred))
+
+#%% evaluation on test set
+y_pred = dt.predict(X_test)
+print('MSE test DT:', mean_squared_error(y_test, y_pred))
+
+"""
+From all the results we can get around MSE of about 0.005 - 0.006 for the test
+set. The aim now is to use all the models to get a better overall result using
+stacking.
+"""
+
+
